4a1dc15f3801829578084c71c386d495e76c8ff9
dkirby <root@ip-10-194-98-3.ec2.internal>
2011-09-27 11:24:05 -0400

    Added my-first-cookbook and helloworld recipe

---
+++ cookbooks/my-first-cookbook/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "YOUR_COMPANY_NAME"
+maintainer_email "YOUR_EMAIL"
+license          "All rights reserved"
+description      "Installs/Configures my-first-cookbook"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+recipe	"my-first-cookbook':helloworld", "My first recipe, prints Hello World to everyone."
